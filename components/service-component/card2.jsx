e12e227a0ca4f2caca283be53bb7fd367163b4b1
ahmetkrcc0 <116486929+ahmetkrcc0@users.noreply.github.com>
2023-12-08 21:23:32 +0300

    deneme1

---
+++ components/service-component/card2.jsx
@@ -0,0 +1,153 @@
+import {
+    Accordion,
+    AccordionItem,
+    AccordionButton,
+    AccordionPanel,
+    AccordionIcon,
+    Box,
+  } from "@chakra-ui/react";
+  import "./card2.css";
+  import {
+    FaArrowRight,
+    FaEnvelope,
+    FaMapMarkerAlt,
+    FaPhone,
+  } from "react-icons/fa";
+  import { Link } from "react-router-dom";
+  import FreeTrial from "../free-trial-component/freeTrial";
+  import { motion } from "framer-motion";
+  import { animationVariants } from "../../constants/animationVariants";
+  import { scrollToTop } from "../../constants/scrollToTop";
+  const card2 = ({ id, title, breif, descr, imageSrc }) => {
+    return (
+      <>
+        <div
+          style={{
+            backgroundImage: `url(${imageSrc})`,
+          }}
+          className="relative bg-no-repeat bg-center bg-cover pt-44 pb-36 max-md:pt-36 max-md:pb-24 flex justify-center items-center text-white "
+        >
+          <div className="absolute top-0 bottom-0 left-0 right-0 bg-black/50"></div>
+          <motion.h1
+            initial="initial"
+            whileInView="animate"
+            variants={animationVariants.zoomOut}
+            viewport={{ once: true, amount: 0.2 }}
+            className="text-6xl max-md:text-5xl max-sm:text-4xl font-semibold z-10"
+          >
+            {title}
+          </motion.h1>
+        </div>
+        <div
+          style={{ maxWidth: 1200 }}
+          className=" mx-auto flex
+           p-10 max-sm:px-5 relative items-start gap-16 max-lg:flex-col"
+        >
+  
+          <motion.div
+            initial="initial"
+            whileInView="animate"
+            variants={animationVariants.zoomIn}
+            viewport={{ once: true, amount: 0.2 }}
+            className="w-1/3 max-lg:w-full sticky top-36 questions-card p-8 gap-5 flex flex-col"
+          >
+  
+              
+            <h1 className="text-center text-2xl font-semibold">
+              WordPress oluşturma
+            </h1>
+            
+            <div className="card-header text-2xl flex items-center gap-4">
+             
+              
+            </div>
+            <div className=" w-1/1 max-md:w-full flex flex-col text-white items-start justify-between max-md:items-center">
+                <img src="/12.png" alt="" />
+              </div>
+              <p> DETAY </p>
+            <Link
+              to="/contact"
+              onClick={scrollToTop}
+              className="text-red-500 hover:text-black transition-all duration-300"
+            >
+              <div className="flex items-center gap-1">
+                <p className="text-xl">Contact us</p>
+                <FaArrowRight className="text-sm mt-[2px]" />
+              </div>
+            </Link>
+  
+          </motion.div>
+          <motion.div
+            initial="initial"
+            whileInView="animate"
+            variants={animationVariants.zoomIn}
+            viewport={{ once: true, amount: 0.2 }}
+            className="w-1/3 max-lg:w-full sticky top-36 questions-card p-8 gap-5 flex flex-col"
+          >
+  
+              
+            <h1 className="text-center text-2xl font-semibold">
+              WordPress oluşturma
+            </h1>
+            
+            <div className="card-header text-2xl flex items-center gap-4">
+             
+              
+            </div>
+            <div className=" w-1/1 max-md:w-full flex flex-col text-white items-start justify-between max-md:items-center">
+                <img src="/12.png" alt="" />
+              </div>
+              <p> DETAY </p>
+            <Link
+              to="/contact"
+              onClick={scrollToTop}
+              className="text-red-500 hover:text-black transition-all duration-300"
+            >
+              <div className="flex items-center gap-1">
+                <p className="text-xl">Contact us</p>
+                <FaArrowRight className="text-sm mt-[2px]" />
+              </div>
+            </Link>
+          </motion.div>
+          <motion.div
+            initial="initial"
+            whileInView="animate"
+            variants={animationVariants.zoomIn}
+            viewport={{ once: true, amount: 0.2 }}
+            className="w-1/3 max-lg:w-full sticky top-36 questions-card p-8 gap-5 flex flex-col"
+          >
+  
+              
+            <h1 className="text-center text-2xl font-semibold">
+              WordPress oluşturma
+            </h1>
+            
+            <div className="card-header text-2xl flex items-center gap-4">
+             
+              
+            </div>
+            <div className=" w-1/1 max-md:w-full flex flex-col text-white items-start justify-between max-md:items-center">
+                <img src="/12.png" alt="" />
+              </div>
+              <p> DETAY </p>
+            <Link
+              to="/contact"
+              onClick={scrollToTop}
+              className="text-red-500 hover:text-black transition-all duration-300"
+            >
+              <div className="flex items-center gap-1">
+                <p className="text-xl">Contact us</p>
+                <FaArrowRight className="text-sm mt-[2px]" />
+              </div>
+            </Link>
+          </motion.div>
+        
+          
+        </div>
+        <FreeTrial />
+      </>
+    );
+  };
+  
+  export default card2;
+  
